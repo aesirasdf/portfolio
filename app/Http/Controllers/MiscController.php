558da9a019788938eb6c663b91dd1566764f079b
aesirasdf <aerenesenpai@gmail.com>
2025-02-13 22:22:21 +0800

    cache added

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use OpenAI;
 
 class MiscController extends Controller
@@ -10,21 +11,24 @@ class MiscController extends Controller
     
     public function status(){
         return response()->json([
-            'ok' => true
+            'ok' => true,
+            "your ip" => request()->ip()
         ], 200);
     }
 
     public function valentines(){
-        $client = OpenAI::client(env("OPENAI_API_KEY"));
-        $result = $client->chat()->create([
-            "model" => "gpt-4o-mini",
-            "messages" => [
-                ["role" => "system", "content" => "remove the beginning and end of your message. make it in json format with title and message key. add humor to the message. this message will be posted at school"],
-                ["role" => "user", "content" => "Make a valentines message for . make it random, remove all [your name] make it generic and add from MIS Department"]
-            ]
-        ]);
-
-
+        $result = Cache::remember(request()->ip(), now()->addHour(), function(){
+            $client = OpenAI::client(env("OPENAI_API_KEY"));
+            return $client->chat()->create([
+                "model" => "gpt-4o-mini",
+                "messages" => [
+                    ["role" => "system", "content" => "remove the beginning and end of your message. make it in json format with title and message key. add humor to the message. this message will be posted at school"],
+                    ["role" => "user", "content" => "Make a valentines message for . make it random, remove all [your name] make it generic and add from MIS Department"]
+                ]
+            ]);
+    
+    
+        });
         $data = [
             "ok" => true,
             "data" => json_decode($result->choices[0]->message->content)
--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/inquiries', [InquiryController::class, "store"]);
 
 Route::get("/valentines", [MiscController::class, "valentines"]);
+Route::get("/status", [MiscController::class, "status"]);
