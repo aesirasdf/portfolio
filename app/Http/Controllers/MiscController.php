bd0f14190a4bbf376b4d019c4e0eb121e23ecdcc
aesirasdf <jecorenzconcepcion@gmail.com>
2025-02-13 16:41:00 +0800

    Valentines added

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use OpenAI;
 
 class MiscController extends Controller
 {
@@ -11,5 +12,21 @@ class MiscController extends Controller
         return response()->json([
             'ok' => true
         ], 200);
+    }
+
+    public function valentines(){
+        $client = OpenAI::client(env("OPENAI_API_KEY"));
+        $result = $client->chat()->create([
+            "model" => "gpt-4o-mini",
+            "messages" => [
+                ["role" => "system", "content" => "remove the beginning and end of your message."],
+                ["role" => "user", "content" => "Make a valentines message for . make it random, remove all [your name] make it generic and add from MIS Department"]
+            ]
+        ]);
+
+
+
+
+        return $result->choices[0]->message->content;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\MiscController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/inquiries', [InquiryController::class, "store"]);
+
+Route::get("/valentines", [MiscController::class, "valentines"]);
