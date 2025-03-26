84f1e64f960b10b51697bec2bdb795333fe1a9e9
aesirasdf <aerenesenpai@gmail.com>
2025-03-26 12:51:56 +0800

    asdf

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -10,6 +10,15 @@ class MiscController extends Controller
 {
     
     public function status(){
+        $client = \OpenAI::factory()->withBaseUri("http://localhost:3000/api")->withApiKey(env("sk"))->make();
+        $res = $client->chat()->create([
+            "model" => "gpt-4o-mini",
+            "messages" => [
+                ["role" => "system", "content" => "remove the beginning and end of your message. make it in json format with title and message key. add humor to the message. this message will be posted at school"],
+                ["role" => "user", "content" => "Make a valentines message for . make it random, remove all [your name] make it generic and add from MIS Department"]
+            ]
+        ]);
+
         return response()->json([
             'ok' => true,
             "your ip" => request()->ip()
